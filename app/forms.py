43514a25a9556276bdcdb753eae2d5058197a91c
= <=>
2025-01-14 14:46:34 +0300

    form and view for rpc calling

---
+++ app/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+import json
+
+class RpcCallForm(forms.Form):
+    method = forms.CharField(label="Method", max_length=100)
+    params = forms.CharField(
+        label="Params (JSON)", 
+        required=False, 
+        widget=forms.Textarea(attrs={"rows": 3})
+    )
+
+    def clean_params(self):
+        params = self.cleaned_data.get('params')
+        if params:
+            try:
+                return json.loads(params)
+            except json.JSONDecodeError:
+                raise forms.ValidationError("Invalid JSON format")
+        return {}
--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
+from django.forms import Form
+from django.views.generic import FormView
 from django.shortcuts import render
 
-# Create your views here.
+from app.utils.web_client import JsonRpcClient
+from .forms import RpcCallForm
+from QualixTT import settings
+
+class RpcCallView(FormView):
+    template_name = "rpc_call.html"
+    form_class = RpcCallForm
+
+    def form_valid(self, form: Form):
+        method = form.cleaned_data['method']
+        params = form.cleaned_data.get('params')
+        client = JsonRpcClient(endpoint=settings.JSONRPC_ENDPOINT)
+        result = client.call_method(method, params)
+        return render(self.request, self.template_name, {
+            'form': form,
+            'result': result
+        })
